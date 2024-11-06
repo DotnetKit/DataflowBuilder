e8f64ec675d722179ca7aa244ed48f4d8bdd6741
medevod <devteam@evodim.com>
2024-11-06 01:24:09 +0100

    chore (reorg): reorganize solution structure

---
+++ src/DataflowBuilder/Abstractions/IPipelineBuilder.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks.Dataflow;
+
+namespace DataflowBuilder.Core.Pipeline
+{
+    public interface IPipelineBuilder<TSource> : IPipelineBuilder
+    {
+        ISourceBlock<TSource> SourceBlock { get; }
+
+        IPipeline<TSource> Build();
+    }
+
+    public interface IPipelineBuilder
+    {
+        CancellationToken CancellationToken { get; }
+
+        void AddPropagator<TInput, TOutput>(IPropagatorBlock<TInput, TOutput> propagatorBlock);
+
+        void AddTarget<TInput>(ITargetBlock<TInput> targetBlock);
+    }
+}
---
+++ src/DataflowBuilder/Core/IFluentBuilder.cs
@@ -0,0 +1,12 @@
+﻿namespace DataflowBuilder.Core.Pipeline
+{
+    public interface IFluentBuilder<out TContext>
+    {
+        TContext Current { get; }
+    }
+
+    public interface IFluentBuilder<in TExtension, out TContext>
+    {
+        TContext Current { get; }
+    }
+}

daee200db1bdd95b200c47c40ec8158ff7d16339
medevod <devteam@evodim.com>
2024-12-05 21:45:53 +0100

    chore

--- src/DotnetKit.DataflowBuilder/Abstractions/IPipeline.cs
+++ src/DotnetKit.DataflowBuilder/Abstractions/IPipeline.cs
@@ -1,5 +1,12 @@
 ﻿namespace DotnetKit.DataflowBuilder.Abstractions;
 
+/// <summary>
+///   Represents a pipeline abstraction. 
+/// </summary>
+/// <typeparam name="TSource"></typeparam> <summary>
+/// 
+/// </summary>
+/// <typeparam name="TSource"></typeparam>
 public interface IPipeline<TSource>
 {
     Task CompleteAsync();
